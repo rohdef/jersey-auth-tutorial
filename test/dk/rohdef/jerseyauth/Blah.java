ca0a5b237753fa27389d66d7910b231215310d02
Rohde Fischer <rohdef@rohdef.dk>
2015-08-19 14:45:47 +0200

    Sample test

---
+++ test/dk/rohdef/jerseyauth/Blah.java
@@ -0,0 +1,22 @@
+package dk.rohdef.jerseyauth;
+
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * License MIT
+ *
+ * @author Rohde Fischer
+ */
+public class Blah {
+    @Test
+    public void FooBar() {
+        assertThat("True is true", true);
+    }
+
+    @Test
+    public void BarFoo() {
+        assertThat("True is false", true, is(not(true)));
+    }
+}
